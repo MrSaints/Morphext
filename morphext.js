7fb4d6a5df80b9f1293aed6e7a6aa00728a522e4
MrSaints <ian@fyianlai.com>
2014-03-09 18:26:38 +0000

    Fixed indentation

--- morphext.js
+++ morphext.js
@@ -10,64 +10,64 @@
  * http://ian.mit-license.org/
  */
 ;(function ($, window, document, undefined) {
-	var pluginName = "Morphext",
-		defaults = {
-			animation: "bounceIn",
-			separator: ",",
-			speed: 2000
-		};
+    var pluginName = "Morphext",
+        defaults = {
+            animation: "bounceIn",
+            separator: ",",
+            speed: 2000
+        };
 
-	function Plugin (element, options) {
-		this.element = $(element);
-		
-		this.settings = $.extend({}, defaults, options);
-		this._defaults = defaults;
-		this._name = pluginName;
-		this.init();
-	}
+    function Plugin (element, options) {
+        this.element = $(element);
+        
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
 
-	Plugin.prototype = {
-		init: function () {
-			var $that = this;
-			this.phrases = [];
+    Plugin.prototype = {
+        init: function () {
+            var $that = this;
+            this.phrases = [];
 
-			$.each(this.element.text().split(this.settings.separator), function (key, value) {
-				$that.phrases.push(value);
-			});
+            $.each(this.element.text().split(this.settings.separator), function (key, value) {
+                $that.phrases.push(value);
+            });
 
-			this.current = this.phrases[0];
+            this.current = this.phrases[0];
 
-			this.animate();
+            this.animate();
 
-			setInterval(function () {
-				$that.animate();
-			}, this.settings.speed);
-		},
-		animate: function () {
-			this._reset();
+            setInterval(function () {
+                $that.animate();
+            }, this.settings.speed);
+        },
+        animate: function () {
+            this._reset();
 
-			if (typeof this.next !== "undefined" && this.next !== null)
-				this.current = this.next;
+            if (typeof this.next !== "undefined" && this.next !== null)
+                this.current = this.next;
 
-			this.index = $.inArray(this.current, this.phrases);
-			if ((this.index + 1) == this.phrases.length) // Loop
-				this.index = -1;
+            this.index = $.inArray(this.current, this.phrases);
+            if ((this.index + 1) == this.phrases.length) // Loop
+                this.index = -1;
 
-			this.next = this.phrases[this.index + 1];
+            this.next = this.phrases[this.index + 1];
 
-			$("<span class='animated " + this.settings.animation + "'>" + this.current + "</span>")
-				.appendTo(this.element);
-		},
-		_reset: function () {
-			this.element.html('');
-		}
-	};
+            $("<span class='animated " + this.settings.animation + "'>" + this.current + "</span>")
+                .appendTo(this.element);
+        },
+        _reset: function () {
+            this.element.html('');
+        }
+    };
 
-	$.fn[pluginName] = function (options) {
-		return this.each(function() {
-			if (!$.data(this, "plugin_" + pluginName)) {
-				$.data(this, "plugin_" + pluginName, new Plugin(this, options));
-			}
-		});
-	};
+    $.fn[pluginName] = function (options) {
+        return this.each(function() {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            }
+        });
+    };
 })(jQuery, window, document);
