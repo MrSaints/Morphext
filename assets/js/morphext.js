8dc826a94863506694c68161a26d01320fe713fb
MrSaints <ian@fyianlai.com>
2014-07-08 23:45:04 +0800

    Updated dependencies; improved clarity; added syntax highlighting; and added animation viewer/tester/changer...

--- assets/js/morphext.js
+++ assets/js/morphext.js
@@ -1,1 +1,11 @@
-/*! Morphext - v2.0.0 - 2014-03-20 */!function(a){function b(b,e){this.element=a(b),this.settings=a.extend({},d,e),this._defaults=d,this._name=c,this.init()}var c="Morphext",d={animation:"bounceIn",separator:",",speed:2e3};b.prototype={init:function(){var b=this;this.phrases=[],this.element.addClass("morphext"),a.each(this.element.text().split(this.settings.separator),function(a,c){b.phrases.push(c)}),this.element.html("<span>"+this.phrases.join("</span><span>")+"</span>"),this.index=-1,this.animate(),setInterval(function(){b.animate()},this.settings.speed)},animate:function(){this.index+1===this.phrases.length&&(this.index=-1),++this.index,this.element.find("span").removeClass().eq(this.index).addClass("animated "+this.settings.animation)}},a.fn[c]=function(d){return this.each(function(){a.data(this,"plugin_"+c)||a.data(this,"plugin_"+c,new b(this,d))})}}(jQuery,window,document);
+/*!
+ * Morphext v2.1.0 - Text Rotating Plugin for jQuery
+ * https://github.com/MrSaints/Morphext
+ *
+ * Built on jQuery Boilerplate
+ * http://jqueryboilerplate.com/
+ *
+ * Copyright 2013 Ian Lai and other contributors
+ * Released under the MIT license
+ * http://ian.mit-license.org/
+ */!function(a,b,c,d){function g(b,c){this.element=a(b),this.settings=a.extend({},f,c),this._defaults=f,this._name=e,this.init()}var e="Morphext",f={animation:"bounceIn",separator:",",speed:2e3};g.prototype={init:function(){var b=this;this.phrases=[],this.element.addClass("morphext"),a.each(this.element.text().split(this.settings.separator),function(a,c){b.phrases.push(c)}),this.element.html("<span>"+this.phrases.join("</span><span>")+"</span>"),this.index=-1,this._animate(),setInterval(function(){b._animate()},this.settings.speed)},_animate:function(){this.index+1===this.phrases.length&&(this.index=-1),++this.index,this.element.find("span").removeClass().eq(this.index).addClass("animated "+this.settings.animation)},setAnimation:function(a){this.settings.animation=a}},a.fn[e]=function(b){var c=arguments;if(b===d||"object"==typeof b)return this.each(function(){a.data(this,"plugin_"+e)||a.data(this,"plugin_"+e,new g(this,b))});if("string"==typeof b&&"_"!==b[0]&&"init"!==b){var f;return this.each(function(){var d=a.data(this,"plugin_"+e);d instanceof g&&"function"==typeof d[b]&&(f=d[b].apply(d,Array.prototype.slice.call(c,1))),"destroy"===b&&a.data(this,"plugin_"+e,null)}),f!==d?f:this}}}(jQuery,window,document);
